482a651089ee8b625b8ea35d2736bdb511788b52
Esteban Gomezllata Marmolejo <esteban.gomezllatamarmolejo@protonmail.com>
2016-09-06 00:41:19 +0100

    Formatting, ignoring IntelliJ's files.

--- core/src/main/scala/Mmhope.scala
+++ core/src/main/scala/Mmhope.scala
@@ -3,5 +3,5 @@ package com.agecaf.mmhope
 import com.badlogic.gdx.Game
 
 class Mmhope extends Game {
-    override def create() {}
+  override def create() {}
 }
--- desktop/src/main/scala/Main.scala
+++ desktop/src/main/scala/Main.scala
@@ -3,10 +3,10 @@ package com.agecaf.mmhope
 import com.badlogic.gdx.backends.lwjgl._
 
 object Main extends App {
-    val cfg = new LwjglApplicationConfiguration
-    cfg.title = "mmhope"
-    cfg.height = 480
-    cfg.width = 800
-    cfg.forceExit = false
-    new LwjglApplication(new Mmhope, cfg)
+  val cfg = new LwjglApplicationConfiguration
+  cfg.title = "mmhope"
+  cfg.height = 480
+  cfg.width = 800
+  cfg.forceExit = false
+  new LwjglApplication(new Mmhope, cfg)
 }
