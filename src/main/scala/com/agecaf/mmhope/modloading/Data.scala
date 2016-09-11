8f9cd8e4321fade7217dbe5e1c89f60493ae236c
Esteban Gomezllata Marmolejo <esteban.gomezllatamarmolejo@protonmail.com>
2016-09-11 11:02:39 +0100

    added first version of RGB.

--- src/main/scala/com/agecaf/mmhope/modloading/Data.scala
+++ src/main/scala/com/agecaf/mmhope/modloading/Data.scala
@@ -1,5 +1,8 @@
 package com.agecaf.mmhope.modloading
 
+import com.badlogic.gdx.graphics.Color
+import scala.language.implicitConversions
+
 object Data {
   case class ModIndex(
       id: String,
@@ -48,4 +51,44 @@ object Data {
         sounds &~ other.sounds
       )
   }
+
+  case class Font(
+    path: String,
+    borderWidth: Int = 0,
+    borderColor: String = "black",
+    borderStraight: Boolean = false,
+    shadowOffsetX: Int = 0,
+    shadowOffsetY: Int = 0,
+    shadowColor: String = "black-0.75"
+  )
+
+  case class RGB(r: Float, g: Float, b: Float) {
+    def clamp = RGB (
+      r = if (r > 0) if (r < 1) r else 1 else 0,
+      g = if (g > 0) if (g < 1) g else 1 else 0,
+      b = if (b > 0) if (b < 1) b else 1 else 0
+    )
+
+    def + (other: RGB) = RGB(
+      r + other.r,
+      g + other.g,
+      b + other.b
+    ).clamp
+
+    def - (other: RGB) = RGB(
+      r - other.r,
+      g - other.g,
+      b - other.b
+    ).clamp
+
+    def * (other: RGB) = RGB(
+      r * other.r,
+      g * other.g,
+      b * other.b
+    ).clamp
+
+    def withAlpha (alpha: Float) = new Color(r, g, b, alpha)
+  }
+
+  implicit def RGBtoColor(c: RGB): Color = c.withAlpha(1)
 }
